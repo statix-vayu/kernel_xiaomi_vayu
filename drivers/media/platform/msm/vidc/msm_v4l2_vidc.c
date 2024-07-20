23a07c7a92ead8cf145f2bec18f09b4bdc7e0064
resist15 <sourav@aospa.co>
2024-07-20 22:17:08 +0530

    msm: vidc: Fix incompatible pointer to integer conversion
    Change-Id: I0b93354737e39eeb46096cbcab4035b78ad53216

--- drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
+++ drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
@@ -623,7 +623,7 @@ static int msm_vidc_probe_vidc_device(struct platform_device *pdev)
 	list_add_tail(&core->list, &vidc_driver->cores);
 	mutex_unlock(&vidc_driver->lock);
 
-	core->debugfs_root = msm_vidc_debugfs_init_core(
+	core->debugfs_root = (struct dentry *)msm_vidc_debugfs_init_core(
 		core, vidc_driver->debugfs_root);
 
 	vidc_driver->sku_version = core->resources.sku_version;
@@ -900,7 +900,7 @@ static int __init msm_vidc_init(void)
 
 	INIT_LIST_HEAD(&vidc_driver->cores);
 	mutex_init(&vidc_driver->lock);
-	vidc_driver->debugfs_root = msm_vidc_debugfs_init_drv();
+	vidc_driver->debugfs_root = (struct dentry *)msm_vidc_debugfs_init_drv();
 	if (!vidc_driver->debugfs_root)
 		dprintk(VIDC_ERR,
 			"Failed to create debugfs for msm_vidc\n");
